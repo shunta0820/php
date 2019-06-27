9e8afd98e612ae02e57d37794593ba0de65acf4e
”shunta <shuntatoukairin1204@gmail.com>
2019-06-27 13:06:20 +0000

    first commit

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+echo 'hello php!';
+echo "\n";
+
+$a = 3;
+$b = 7;
+$result = $a + $b;
+echo $result;
+echo "\n";
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+echo "\n";
+$hello = "Hello,";
+$name = "駿太";
+$world = "'s World!";
+echo $hello.$name.$world;
+echo "\n";
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+echo "\n";
